2be0c9f5b92c4314b27997a0102360c9f28c7ff7
Fernanda <fernanda.arteagag@outlook.com>
2022-08-29 11:48:12 -0500

    Inicio de mi repositorio

---
+++ calculator.py
@@ -0,0 +1,15 @@
+user_name=input('\tBienvenido a la calculadora de IMC (Indice de Masa Corporal). \n\nPor favor ingresa tu nombre: ')
+weight=int(input('¿Cuánto pesas?: '))
+height= float(input('¿Cuánto  mides?: '))
+print('\tCalculando... please wait')
+print('\tCalculando... please wait')
+imc = round(weight/(height**2),2)
+print(f"{user_name}:\n>Tu peso es: {weight}\n>Tu estatura es: {height}\n>Tu IMC es: {imc}")
+if imc<18.5:
+    print('Según tus resultados de IMC tu peso es bajo.')
+elif (imc>=18.5) and (imc<25):
+    print('Según tus resultados de IMC tu peso se considera normal.')
+elif (imc>=25) and (imc<30):
+    print('Según tus resultados de IMC tienes sobrepeso.')
+elif imc>=30:
+    print('Según tus resultados de IMC padeces de obesidad.')
